8d0a7e0e10f2117eccfdd1ea01315ac985f358a5
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-02 17:26:39 -0600

    computed properties for objects

--- object_methods/app.js
+++ object_methods/app.js
@@ -1,18 +1,18 @@
 // Shorthand Properties
 
 // old way
-// const getStats = (arr) => {
-//   const max = Math.max(...arr);
-//   const min = Math.min(...arr);
-//   const sum = arr.reduce((sum, val) => sum + val);
-//   const avg = sum / arr.length;
-//   return {
-//     max: max,
-//     min: min,
-//     sum: sum,
-//     avg: avg
-//   };
-// };
+const getStatsOld = (arr) => {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const sum = arr.reduce((sum, val) => sum + val);
+  const avg = sum / arr.length;
+  return {
+    max: max,
+    min: min,
+    sum: sum,
+    avg: avg
+  };
+};
 
 // new way with shorthand properties
 const getStats = (arr) => {
@@ -31,3 +31,34 @@ const getStats = (arr) => {
 const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
 
 const stats = getStats(reviews);
+
+// Computed Properties
+
+const role = "host";
+const person = "Jools Holland";
+
+const role2 = "Director";
+const person2 = "James Cameron";
+
+// old way
+const oldTeam = {};
+oldTeam[role] = person;
+oldTeam[role2] = person2;
+
+// new way with computed properties
+const team = {
+  [role]: person,
+  [role2]: person2
+};
+
+// const addProp = (obj, k, v) => {
+//   const copy = {
+//     ...obj
+//   };
+//   copy[k] = v;
+//   return copy;
+// };
+
+const addProp = (obj, k, v) => ({...obj, [k]: v});
+
+const res = addProp(team, "happy", ":)");
