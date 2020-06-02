387d480529d5cb29673f1b6a378d5fc4c67eb8f9
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-02 12:20:36 -0600

    Spread

--- misc_features/app.js
+++ misc_features/app.js
@@ -18,3 +18,54 @@ const greet = (person, greeting="Hi") => {
 const blah = (x, y=[1,2,3]) => {
   console.log(x, y);
 };
+
+// Spread
+
+const giveMeFour = (a, b, c, d) => {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+  console.log("d", d);
+};
+
+const colors = ["red", "yellow", "orange", "green"];
+
+const str = "GOAT";
+
+const cephalopods = ["dumbo octopus", "humboldt squid", "flamboyant cuttlefish"];
+const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
+const cnidaria = ["fire coral", "moon jelly"];
+
+const mollusca = [...cephalopods, ...gastropods];
+const inverts = [...cnidaria, ...gastropods, ...cephalopods];
+
+const feline = {
+  legs: 4,
+  family: "Felidae"
+};
+
+const canine = {
+  family: "Caninae",
+  furry: true
+};
+
+const dog = {
+  ...canine,
+  isPet: true,
+  adorable: true
+};
+
+const houseCat = {
+  ...feline,
+  isGrumpy: true,
+  personality: "unpredictable"
+};
+
+const catDog = {
+  ...canine,
+  ...feline
+};
+
+const catDogClone = {
+  ...catDog
+};
