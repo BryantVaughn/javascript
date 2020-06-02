67cedd77a1a5574347ddc5ae9483e3004a9aee02
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-01 23:46:00 -0600

    sort function with numbers

--- collection_callbacks/app.js
+++ collection_callbacks/app.js
@@ -164,3 +164,14 @@ const someStartWithD = words.some(word => word[0] === 'd');
 const anyTwoAuthors = books.some(book => (
   book.authors.length === 2
 ));
+
+// Sort
+
+const prices = [400.50, 3000, 99.99, 35.99, 12.00, 9500];
+
+const badSort = prices.slice().sort();
+
+const ascSort = prices.slice().sort((a, b) => a - b);
+const descSort = prices.slice().sort((a, b) => b - a);
+
+books.sort((a, b) => a.rating - b.rating);
