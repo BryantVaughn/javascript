e2b34abdae14caff0dc25ac458aaa16b3c4200f5
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-01 23:46:55 -0600

    note for using slice with sort

--- collection_callbacks/app.js
+++ collection_callbacks/app.js
@@ -171,6 +171,7 @@ const prices = [400.50, 3000, 99.99, 35.99, 12.00, 9500];
 
 const badSort = prices.slice().sort();
 
+// slice makes copy of prices array
 const ascSort = prices.slice().sort((a, b) => a - b);
 const descSort = prices.slice().sort((a, b) => b - a);
 
