9145773fa24d9c89b56af8ca3cce0cfa3204c943
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-04 15:41:35 -0600

    getElementsByTagName()

--- dom_manipulation/assessment/app.js
+++ dom_manipulation/assessment/app.js
@@ -5,3 +5,11 @@ const p = document.getElementById("main");
 
 console.dir(img);
 console.dir(p);
+
+// getElementsByTagName()
+
+const inputs = document.getElementsByTagName("input");
+const items = document.getElementsByTagName("li");
+
+console.dir(inputs);
+console.dir(items);
