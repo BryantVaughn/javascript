df9cbffa2840c033dc32cd61b373327475fd6171
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-04 15:56:08 -0600

    getElementsByClassName() and drilling down if only certain tag types are wanted

--- dom_manipulation/assessment/app.js
+++ dom_manipulation/assessment/app.js
@@ -13,3 +13,15 @@ const items = document.getElementsByTagName("li");
 
 console.dir(inputs);
 console.dir(items);
+
+// getElementsByClassName()
+
+const header = document.getElementsByClassName("header");
+const special = document.getElementsByClassName("special");
+
+// grab only the li's with class of special
+const ul = document.getElementsByTagName("ul")[0];
+console.log(ul.getElementsByClassName("special"));
+
+console.dir(header);
+console.dir(special);
