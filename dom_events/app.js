e1e859a4e57520c407119aeee4f79fcfd76c5fd1
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-07 13:53:39 -0600

    addEventListener

--- dom_events/app.js
+++ dom_events/app.js
@@ -4,12 +4,37 @@
 // input              hits return       get search results
 // image              mouseover         display the img caption
 
-const clicker = document.querySelector("#clicker");
+// This only allows a single click listener when not using addEventListener
 
-clicker.onclick = () => {
-  console.log("YOU CLICKED THE CLICKER!!!");
-};
+// const clicker = document.querySelector("#clicker");
 
-clicker.ondblclick = () => {
-  console.log("YOU DOUBLE CLICKED THE CLICKER!!!!!!!");
-};
+// clicker.onclick = () => {
+//   console.log("YOU CLICKED THE CLICKER!!!");
+// };
+
+// clicker.ondblclick = () => {
+//   console.log("YOU DOUBLE CLICKED THE CLICKER!!!!!!!");
+// };
+// const btn = document.querySelector("button");
+// btn.onclick = () => {
+//   console.log("YOU CLICKED ME!");
+// };
+
+// addEventListener allows multiple of the same event on one element
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+  alert("CLICKED!!!");
+});
+
+btn.addEventListener("click", function() {
+  console.log("Second thing");
+});
+
+btn.addEventListener("mouseover", function() {
+  btn.innerText = "STOP TOUCHING ME!";
+});
+
+btn.addEventListener("mouseout", function() {
+  btn.innerText = "Click Me";
+});
