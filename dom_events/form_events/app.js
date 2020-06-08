b431641d380a4e885e3ac2e4d36a432061012b1a
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-08 12:31:48 -0600

    input and change events

--- dom_events/form_events/app.js
+++ dom_events/form_events/app.js
@@ -11,3 +11,35 @@ form.addEventListener("submit", function(evt) {
   alert("SUBMITTED THE FORM!");
 });
 
+const formData = {};
+// input event updates on each keypress
+// for(let input of [creditCard, termsCheckbox, veggieSelect]) {
+//   input.addEventListener("input", ({ target }) => {
+//     const {name, type, value, checked} = target;
+//     formData[name] = type === "checkbox" ? checked : value;
+//   });
+// }
+
+// change event doesn't update creditcard until the input isn't in focus
+for(let input of [creditCard, termsCheckbox, veggieSelect]) {
+  input.addEventListener("change", ({ target }) => {
+    const {name, type, value, checked} = target;
+    formData[name] = type === "checkbox" ? checked : value;
+    console.log(formData);
+  });
+}
+
+// creditCard.addEventListener("input", (evt) => {
+//   console.log("Input changed");
+//   formData["cc"] = evt.target.value;
+// });
+
+// veggieSelect.addEventListener("input", (evt) => {
+//   console.log("VEGGIE!!!", evt);
+//   formData["veggie"] = evt.target.value;
+// });
+
+// termsCheckbox.addEventListener("input", (evt) => {
+//   console.log("CHECKED!", evt);
+//   formData["agreeToTerms"] = evt.target.checked;
+// });
