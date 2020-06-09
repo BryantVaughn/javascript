d63b45b0b1590b6020534b4aac8a9786534f07a3
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-09 12:50:30 -0600

    resolve/reject with values for then and catch

---
+++ async_code/promises/app2.js
@@ -0,0 +1,42 @@
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const pages = {
+        "/users": [
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" }
+        ],
+        "/about": "This is the about page!"
+      };
+      const data = pages[url];
+      if(data) {
+        resolve({ status: 200, data });
+      }
+      else {
+        reject({ status: 404 });
+      }
+    }, 1000);
+  });
+};
+
+fakeRequest("/projects")
+  .then((res) => {
+    console.log("Status Code:", res.status);
+    console.log("Data:", res.data);
+    console.log("REQUEST WORKED!");
+  })
+  .catch((res) => {
+    console.log(res.status);
+    console.log("REQUEST FAILED");
+  });
+
+  fakeRequest("/users")
+    .then((res) => {
+      console.log("Status Code:", res.status);
+      console.log("Data:", res.data);
+      console.log("REQUEST WORKED!");
+    })
+    .catch((res) => {
+      console.log(res.status);
+      console.log("REQUEST FAILED");
+    });
