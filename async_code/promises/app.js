95e76ff00dd810e6ec9a869ee41db5f21570ca10
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-09 12:11:10 -0600

    intro to promises in js

---
+++ async_code/promises/app.js
@@ -0,0 +1,27 @@
+// const willGetYouADog = new Promise((resolve, reject) => {
+//   const rand = Math.random();
+//   if(rand < 0.5) resolve();
+//   else reject();
+// })
+// .then(() => {
+//   console.log("YAY WE GOT A DOG!");
+// })
+// .catch(() => {
+//   console.log(":( NO DOG");
+// });
+const makeDogPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
+      if(rand < 0.5) resolve();
+      else reject();
+    }, 5000);
+  })
+};
+makeDogPromise()
+  .then(() => {
+    console.log("YAY WE GOT A DOG!");
+  })
+  .catch(() => {
+    console.log(":( NO DOG");
+  });
