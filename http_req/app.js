3604eac4d3269e54571ef62d43a44cf3b5974dd2
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-09 15:45:58 -0600

    using response data to make second request with XMLHttpRequest

--- http_req/app.js
+++ http_req/app.js
@@ -2,14 +2,22 @@ const myReq = new XMLHttpRequest();
 
 myReq.onload = function() {
   const data = JSON.parse(this.responseText);
-  // for(let i = 0; i < data.results.length; i++) {
-  //   const {name, diameter, climate} = data.results[i];
+  const filmUrl = data.results[0].films[0];
+  const filmRequest = new XMLHttpRequest();
+  filmRequest.addEventListener("load", function() {
+    const filmData = JSON.parse(this.responseText);
+    console.log(filmData.title);
+  });
+  filmRequest.addEventListener("error", function(err) {
+    console.log("ERROR!!!", err);
+  });
+  filmRequest.open("get", filmUrl, true);
+  filmRequest.setRequestHeader("Accept", "application/json");
+  filmRequest.send();
+  // for(let planet of data.results) {
+  //   const {name, diameter, climate} = planet;
   //   console.log(`${name} has a diameter of ${diameter} and the climate is ${climate}.`);
   // }
-  for(let planet of data.results) {
-    const {name, diameter, climate} = planet;
-    console.log(`${name} has a diameter of ${diameter} and the climate is ${climate}.`);
-  }
 };
 myReq.onerror = function(err) {
   console.log("ERROR!", err);
