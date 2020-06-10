43972d35be2b4f798406e210d8a4ab65dc994e17
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-10 11:03:39 -0600

    using helper functions to improve readability

--- http_req/fetch/app.js
+++ http_req/fetch/app.js
@@ -1,23 +1,29 @@
-fetch("https://swapi.dev/api/planets/")
-  .then((response) => {
-    if(!response.ok) {
-      throw new Error(`Status Code Error: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const filmUrl = data.results[0].films[0];
-    return fetch(filmUrl);
-  })
-  .then((response) => {
-    if(!response.ok) {
-      throw new Error(`Status Code Error: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((filmData) => {
-    console.log(filmData);
-  })
+// helper functions
+const checkStatus = (response) => {
+  if(!response.ok) {
+    throw new Error(`Status Code Error: ${response.status}`);
+  }
+  return response.json();
+};
+
+const printPlanets = (data) => {
+  console.log(`Loaded ${data.results.length} Planets`);
+  for(let planet of data.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(data.next);
+};
+
+const fetchNextPlanets = (url = "https://swapi.dev/api/planets/") => {
+  return fetch(url);
+};
+
+fetchNextPlanets()
+  .then(checkStatus)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(checkStatus)
+  .then(printPlanets)
   .catch((error) => {
     console.log("SOMETHING WENT WRONG", error);
   });
