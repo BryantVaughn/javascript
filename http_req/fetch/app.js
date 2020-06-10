2ad5fd751e19be48f2a4bcc33afe06e6a7b73d14
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-10 10:36:06 -0600

    chaining requests together using fetch and promises

--- http_req/fetch/app.js
+++ http_req/fetch/app.js
@@ -1,13 +1,22 @@
-fetch("https://swapi.dev/api/planets21/")
+fetch("https://swapi.dev/api/planets/")
   .then((response) => {
     if(!response.ok) {
       throw new Error(`Status Code Error: ${response.status}`);
     }
-    response.json().then((data) => {
-      for(let planet of data.results) {
-        console.log(planet.name);
-      }
-    });
+    return response.json();
+  })
+  .then((data) => {
+    const filmUrl = data.results[0].films[0];
+    return fetch(filmUrl);
+  })
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error(`Status Code Error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((filmData) => {
+    console.log(filmData);
   })
   .catch((error) => {
     console.log("SOMETHING WENT WRONG", error);
