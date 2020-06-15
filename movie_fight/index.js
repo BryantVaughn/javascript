34a4e35660801be1143a4f0cd54992e8a5880a87
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-15 13:53:39 -0600

    connect input with api query string

--- movie_fight/index.js
+++ movie_fight/index.js
@@ -1,12 +1,24 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "1ba7eb86",
-      s: "avengers"
+      s: searchTerm
     }
   });
 
   console.log(response.data);
 }
 
-fetchData();
+const input = document.querySelector("input");
+
+let timeoutId;
+const onInput = (evt) => {
+  // stop timer if user still typing
+  if(timeoutId) clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    fetchData(evt.target.value);
+  }, 500);
+};
+
+input.addEventListener("input", onInput);
