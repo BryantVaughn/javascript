e97c35d06514aa16352d44ac5c869dc71446bd07
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-15 12:08:56 -0600

    connect app to OMDb API

--- movie_fight/index.js
+++ movie_fight/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "1ba7eb86",
+      s: "avengers"
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();
