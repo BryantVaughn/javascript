fba849291d005b56dc339c8bcaac17a3612ebabe
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-16 10:36:05 -0600

    parse number values for movie comparisons

--- movie_fight/index.js
+++ movie_fight/index.js
@@ -27,7 +27,7 @@ createAutoComplete({
   root: document.querySelector("#left-autocomplete"),
   onOptionSelect(movie) {
     document.querySelector(".tutorial").classList.add("is-hidden");
-    onMovieSelect(movie, document.querySelector("#left-summary"));
+    onMovieSelect(movie, document.querySelector("#left-summary"), "left");
   }
 });
 
@@ -37,11 +37,13 @@ createAutoComplete({
   root: document.querySelector("#right-autocomplete"),
   onOptionSelect(movie) {
     document.querySelector(".tutorial").classList.add("is-hidden");
-    onMovieSelect(movie, document.querySelector("#right-summary"));
+    onMovieSelect(movie, document.querySelector("#right-summary"), "right");
   }
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "1ba7eb86",
@@ -50,9 +52,26 @@ const onMovieSelect = async (movie, summaryElement) => {
   });
 
   summaryElement.innerHTML = movieTemplate(response.data);
+  if (side === "left") leftMovie = response.data;
+  else rightMovie = response.data;
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {
+  console.log("Time for comparison!");
 };
 
 const movieTemplate = movieDetail => {
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+
+  console.log(metascore, imdbRating, imdbVotes);
+  console.log(movieDetail);
+  
   return `
     <article class="media">
       <figure class="media-left">
@@ -69,6 +88,7 @@ const movieTemplate = movieDetail => {
         </div>
       </div>
     </article>
+
     <article class="notification is-primary">
       <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards</p>
