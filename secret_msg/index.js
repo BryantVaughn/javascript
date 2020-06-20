1496b6a57e2843fa3ad40128e0108dae14055496
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-19 18:19:39 -0600

    create link with hash value equal to encrypted message

--- secret_msg/index.js
+++ secret_msg/index.js
@@ -1,6 +1,11 @@
 document.querySelector("form").addEventListener("submit", (evt) => {
   evt.preventDefault();
 
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+  
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
