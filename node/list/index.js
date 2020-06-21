5a6225c051f466c68c48a346ec44aaec413040ec
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-21 15:32:28 -0600

    use isFile to check for file and directory type and refactor callback

--- node/list/index.js
+++ node/list/index.js
@@ -5,5 +5,34 @@ const fs = require("fs");
 fs.readdir(process.cwd(), (err, files) => {
   if (err) throw new Error(err);
 
-  console.log(files);
+  // BAD CODE TO FIND IF FILE OR DIRECTORY
+  // for (let file of files) {
+  //   fs.lstat(file, (err, stats) => {
+  //     if (err) throw new Error(err);
+
+  //     console.log(file, stats.isFile());
+  //   });
+  // }
+
+  // Callback-Based Approach #1
+  const allStats = Array(files.length).fill(null);
+
+  for (let file of files) {
+    const index = files.indexOf(file);
+    fs.lstat(file, (err, stats) => {
+      if (err) throw new Error(err);
+
+      allStats[index] = stats;
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(files[index], stats.isFile());
+        });
+      }
+    });
+  }
+
+  
 });
