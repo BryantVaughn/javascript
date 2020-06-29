12525d284df2a02fd3eec3df38ffd453eb8b57f8
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-29 11:12:51 -0600

    implement map function based on test

--- hidash/index.js
+++ hidash/index.js
@@ -3,5 +3,12 @@ module.exports = {
     for (let idx in arr) {
       fn(arr[idx], idx);
     }
+  },
+  map(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(fn(arr[i], i));
+    }
+    return result;
   }
 };
