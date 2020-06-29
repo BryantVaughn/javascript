b7882eeb06bab10a264ebb8d912c97d8d190be5d
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-29 16:10:06 -0600

    initial implementation of test for autocomplete widget

--- movies/test/autocomplete.test.js
+++ movies/test/autocomplete.test.js
@@ -1,3 +1,15 @@
-it("works", () => {
-
+it("Shows an autocomplete", () => {
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Some other movie" },
+        { Title: "Not Avengers" }
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
 });
