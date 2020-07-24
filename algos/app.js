dffe875630c4bea952310a5f7fe53a469d552fdc
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-24 14:46:16 -0600

    two approaches to summing 1 to n

---
+++ algos/app.js
@@ -0,0 +1,12 @@
+// Add all numbers from to to n inclusive
+function addUpTo(n) {
+	return (n * (n + 1)) / 2;
+
+	// let total = 0;
+	// for (let i = 1; i <= n; i++) {
+	//   total += i;
+	// }
+	// return total;
+}
+
+console.log(addUpTo(6));
