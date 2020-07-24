b1fec663514cf6480c725957f4c6993e2b857e56
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-24 15:04:00 -0600

    notes

--- algos/app.js
+++ algos/app.js
@@ -1,7 +1,9 @@
 // Add all numbers from to to n inclusive
 function addUpTo(n) {
+	// constant operations no matter the size of n
 	return (n * (n + 1)) / 2;
 
+	// operations are linked to size of n
 	// let total = 0;
 	// for (let i = 1; i <= n; i++) {
 	// 	total += i;
